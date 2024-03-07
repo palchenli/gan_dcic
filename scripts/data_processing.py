2cf4c52d2853c50af3aca0dd4d510664aa0d6c3b
palchenli <101778195+palchenli@users.noreply.github.com>
2024-03-07 11:42:26 +0800

    update

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -12,10 +12,14 @@ if __name__ == "__main__":
             file_name = "0" + file_name
 
         file_name = "A" + file_name + ".txt"
-        with open("../data/original_data/case/" + file_name, encoding="GB2312") as f:
-            print("*"*50)
-            for line in f.readlines():
-                print(line)
+
+        with open("../data/original_data/case/" + file_name, "rb") as f:
+            print(chardet.detect(f.read()))
+
+        # with open("../data/original_data/case/" + file_name, encoding="GB2312") as f:
+        #     print("*"*50)
+        #     for line in f.readlines():
+        #         print(line)
 
 
     # question
